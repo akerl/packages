4edaf970de6d90be1d2db3b434c7b3f6d62f7149
Les Aker <me@lesaker.org>
2014-05-31 17:56:42 -0400

    component stuff

--- lib/packages/apt.rb
+++ lib/packages/apt.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'zlib'
 
 module Packages
   ##
@@ -16,16 +17,23 @@ module Packages
     private
 
     def load_repo
-      repo = open(@url + 'Release' ) do |fh|
+      repo = open("#{@url}/Release" ) do |fh|
         Hash[fh.readlines[1..7].map { |x| x.strip.split ': ' }]
       end
       @distro = repo['Label']
       @version = repo['Version']
       @codename = repo['Codename']
       @components = repo['Components'].split
+      unless repo['Architectures'].split.include? @arch
+        fail 'specificed arch not found'
+      end
     end
 
     def load_packages
+      @components.map do |comp|
+        packages = open("#{@url}/#{comp}/binary-#{@arch}/Packages.bz2") do |fh|
+        end
+      end
     end
   end
 end
